797a040db09581f55eb5a85d543d80edeb322dcb
Yuya Niimi <yuu.niimi@gmail.com>
2016-02-22 17:39:38 +0900

    added cc header

--- send_mail.py
+++ send_mail.py
@@ -14,14 +14,17 @@ from email.Utils import formatdate
 
 FROM_ADDRESS = ""
 TO_ADDRESS = ""
+CC_ADDRESS = [""]
 
-def send_mail(from_addr, to_addr, subject, text):
+def send_mail(from_addr, to_addr, cc_addr, subject, text):
     charset = "ISO-2022-JP"
+    cc = ', '.join(cc_addr)
 
     msg = MIMEText(text.encode(charset), "plain", charset)
     msg["Subject"] = Header(subject,charset)
     msg["From"]    = from_addr
     msg["To"]      = to_addr
+    msg["Cc"]      = cc
     msg["Date"]    = formatdate(localtime = True)
 
     smtp = smtplib.SMTP("localhost")
@@ -34,10 +37,11 @@ def send_mail(from_addr, to_addr, subject, text):
 def main():
     from_address = FROM_ADDRESS
     to_address = TO_ADDRESS
+    cc_address = CC_ADDRESS
     subject = "test"
     text = "test"
 
-    send_mail(from_address, to_address, subject, text)
+    send_mail(from_address, to_address, cc_address, subject, text)
 
     return
 
